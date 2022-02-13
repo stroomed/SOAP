ab704ff1605d56ebcf0317a8f687706424cb51bd
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-13 20:43:57 -0300

    Creacion de proyecto

---
+++ Consuming/src/test/java/com/ricardo/consuming/ConsumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.consuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
