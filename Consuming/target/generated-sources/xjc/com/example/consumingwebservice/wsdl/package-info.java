5dbb999af9839803242429deb83cf62ac116538c
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-14 11:41:02 -0300

    Pruebas

--- Consuming/target/generated-sources/xjc/com/example/consumingwebservice/wsdl/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
-// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.02.14 a las 11:12:04 AM CLST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.consumingwebservice.wsdl;
