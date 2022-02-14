c8ecdb71a9671bd1bc1a5ccf836f2e2fe2f08153
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-14 16:12:04 -0300

    Creacion

---
+++ Consuming/src/main/java/com/ricardo/consuming/ConsumingApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.consuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingApplication.class, args);
+	}
+
+}
