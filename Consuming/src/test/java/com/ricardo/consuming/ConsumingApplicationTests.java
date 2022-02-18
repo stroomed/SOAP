5cfa83db2c2d2903d3d7ef3557cf2b595210b7cf
ricardo.gutierrez <ricardo.gutierrez.valejos@gmail.com>
2022-02-18 10:59:05 -0300

    Update ConsumingApplicationTests.java

--- Consuming/src/test/java/com/ricardo/consuming/ConsumingApplicationTests.java
+++ Consuming/src/test/java/com/ricardo/consuming/ConsumingApplicationTests.java
@@ -1,13 +1,49 @@
 package com.ricardo.consuming;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = ConfigurationHello.class, loader = AnnotationConfigContextLoader.class)
+@TestPropertySource("/application.properties")
 class ConsumingApplicationTests {
+	
+	@Autowired
+	private SOAPConnectClient client;
+	
+	@Value("${WSDL.SOAPClient}")
+	private String clientEndPoint;
+	
+	private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	
+	@Before
+	public void init() throws Exception {
+		marshaller.setPackagesToScan("io.spring.guides.gs_producing_web_service");
+		marshaller.afterPropertiesSet();
+	}
 
 	@Test
-	void contextLoads() {
+	void unLockUser() {
+		GetCountryRequest request = new GetCountryRequest();
+		request.setName("Spain");
+		GetCountryResponse response = (GetCountryResponse) client.callWebServices(clientEndPoint, request);
+		assertEquals(true, response.getCountry());
 	}
 
 }
